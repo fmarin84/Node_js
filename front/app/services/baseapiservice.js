95e97974ab777f486a7dbac6b53b83a6c4a73e5c
fmarin84 <fabien.marin84@gmail.com>
2021-05-28 16:16:35 +0200

    conection string

--- front/app/services/baseapiservice.js
+++ front/app/services/baseapiservice.js
@@ -1,5 +1,5 @@
 // const serviceBaseUrl = "http://localhost:3333"
-const serviceBaseUrl = "ec2-23-20-194-1.compute-1.amazonaws.com:3333"
+const serviceBaseUrl = "http://ec2-23-20-194-1.compute-1.amazonaws.com:3333"
 
 class BaseAPIService {
     constructor(url) {
