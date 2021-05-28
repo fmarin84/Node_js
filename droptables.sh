6687376854f3edb115bd84d04111c0071acf8857
fmarin84 <fabien.marin84@gmail.com>
2021-05-28 17:04:47 +0200

    add script to git

---
+++ droptables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "****** Drop tables " "******"
+DROP TABLE public.user_role;
+DROP TABLE public.share;
+DROP TABLE public.payment;
+DROP TABLE public.role;
+DROP TABLE public.item;
+DROP TABLE public.notification;
+DROP TABLE public.list;
+DROP TABLE public.useraccount;
+
+echo "******************************************"
---
+++ gitpull.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo ""
+echo "****** Getting latest for" "******"
+git pull
+echo "******************************************"
