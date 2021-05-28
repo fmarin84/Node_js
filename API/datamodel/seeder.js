76ae66497b9e58d45d6ddc2facdf6e5130e12440
fmarin84 <fabien.marin84@gmail.com>
2021-05-28 16:53:38 +0200

    fix seeder aws

--- API/datamodel/seeder.js
+++ API/datamodel/seeder.js
@@ -8,7 +8,7 @@ module.exports = (userAccountService, listService, itemService, shareService, ro
         try {
             await userAccountService.dao.db.query("CREATE TABLE useraccount(id SERIAL PRIMARY KEY, displayname TEXT NOT NULL, login TEXT NOT NULL, challenge TEXT NOT NULL, isactived BOOLEAN DEFAULT false)")
             await listService.dao.db.query("CREATE TABLE list(id SERIAL PRIMARY KEY, shop TEXT NOT NULL, date DATE, archived BOOLEAN, useraccount_id INTEGER REFERENCES useraccount(id))")
-            await itemService.dao.db.query("CREATE TABLE item(id SERIAL PRIMARY KEY, label TEXT NOT NULL, quantity INTEGER NOT NULL, checked BOOLEAN, fk_id_list INTEGER, FOREIGN KEY (fk_id_list) REFERENCES list(id))")
+            await itemService.dao.db.query("CREATE TABLE item(id SERIAL PRIMARY KEY, label TEXT NOT NULL, quantity INTEGER NOT NULL, checked BOOLEAN, fk_id_list INTEGER)")
             await shareService.dao.db.query("CREATE TABLE share(fk_id_list INTEGER, useraccount_id INTEGER, state INTEGER, PRIMARY KEY (fk_id_list, useraccount_id, state), FOREIGN KEY (fk_id_list) REFERENCES list(id), FOREIGN KEY (useraccount_id) REFERENCES useraccount(id))")
             await roleService.dao.db.query("CREATE TABLE role(id SERIAL PRIMARY KEY, label TEXT NOT NULL, level INTEGER)")
             await roleService.dao.db.query("CREATE TABLE user_role(fk_user_id INTEGER, fk_role_id INTEGER, PRIMARY KEY (fk_user_id, fk_role_id), FOREIGN KEY (fk_user_id) REFERENCES useraccount(id), FOREIGN KEY (fk_role_id) REFERENCES role(id) )")
